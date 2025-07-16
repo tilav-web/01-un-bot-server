63821cfdd12fb994a4b9acbac5a7defa67e4a4ea
tilav-web <shavqiddintilovov0@gmail.com>
2025-07-16 17:52:46 +0500

    .

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://10.106.0.34',
     credentials: true,
   });
   app.use(cookieParser());
